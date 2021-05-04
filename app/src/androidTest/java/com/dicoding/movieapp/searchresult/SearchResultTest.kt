22dc5ea8687ee601c135409c25668e37d91f628d
briancatraguna <briancatraguna@rocketmail.com>
2021-05-04 15:14:28 +0700

    detail movie instrumented testing passed

--- app/src/androidTest/java/com/dicoding/movieapp/searchresult/SearchResultTest.kt
+++ app/src/androidTest/java/com/dicoding/movieapp/searchresult/SearchResultTest.kt
@@ -11,8 +11,7 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.dicoding.movieapp.R
 import com.dicoding.movieapp.home.HomeActivity
 import com.dicoding.movieapp.utils.EspressoIdlingResources
@@ -20,7 +19,7 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
-class SearchResultActivityTest{
+class SearchResultTest{
 
     @Before
     fun setUp(){
@@ -58,6 +57,40 @@ class SearchResultActivityTest{
         onView(ViewMatchers.withText("TV Shows")).perform(click())
         onView(withId(R.id.rv_shows)).check(matches(isDisplayed()))
         onView(withId(R.id.rv_shows)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(1))
+    }
+
+    @Test
+    fun detailMovies(){
+        //Check whether edit text search is displayed and type in 'Mortal Kombat'
+        onView(withId(R.id.edit_text_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.edit_text_search)).perform(typeText("Mortal Kombat"), closeSoftKeyboard())
+        //Check whether search button is displayed and click
+        onView(withId(R.id.btn_search)).check(matches(isDisplayed()))
+        onView(withId(R.id.btn_search)).perform(click())
+        //Click and go to details activity
+        onView(withId(R.id.rv_movies)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+        //Poster
+        onView(withId(R.id.img_poster)).check(matches(isDisplayed()))
+        //Title
+        onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_title)).check(matches(withText("Mortal Kombat")))
+        //Release data
+        onView(withId(R.id.tv_release_date)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_release_date)).check(matches(withText("2021-04-07")))
+        //Rating
+        onView(withId(R.id.tv_rating)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_rating)).check(matches(withText("7.8")))
+        //Overview
+        onView(withId(R.id.tv_overview)).check(matches(isDisplayed()))
+        onView(withId(R.id.tv_overview)).check(matches(withText("Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted by Emperor Shang Tsung's best warrior, Sub-Zero, seeks out and trains with Earth's greatest champions as he prepares to stand against the enemies of Outworld in a high stakes battle for the universe.")))
+        //Length
+        onView(withId(R.id.length)).check(matches(isDisplayed()))
+        onView(withId(R.id.length)).check(matches(withText("110 m")))
+        //Safe for kids
+        onView(withId(R.id.img_safe)).check(matches(isDisplayed()))
+        //Popularity
+        onView(withId(R.id.popularity)).check(matches(isDisplayed()))
+        onView(withId(R.id.popularity)).check(matches(withText("6382.461")))
     }
 
 }
