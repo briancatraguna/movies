7c6a668e392e699ac8df8c986f03635747662b4f
briancatraguna <briancatraguna@rocketmail.com>
2021-05-17 20:23:05 +0700

    Fixed show repository

--- app/src/main/java/com/dicoding/movieapp/data/source/FavoriteShowRepository.kt
+++ app/src/main/java/com/dicoding/movieapp/data/source/FavoriteShowRepository.kt
@@ -8,23 +8,17 @@ import com.dicoding.movieapp.data.source.local.room.shows.TvShowsStarredDao
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-class FavoriteShowRepository(application: Application) {
-    private val mShowsStarredDao: TvShowsStarredDao
-    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+class FavoriteShowRepository(private val showDao: TvShowsStarredDao) {
 
-    init {
-        val db = TvShowRoomDatabase.getDatabase(application)
-        mShowsStarredDao = db.tvShowsStarredDao()
+    fun readAllShows(): LiveData<List<TvShowsRoomEntity>> = showDao.getAllShows()
+
+    suspend fun addShows(show: TvShowsRoomEntity){
+        showDao.insert(show)
     }
 
-    fun getAllShows(): LiveData<List<TvShowsRoomEntity>> = mShowsStarredDao.getAllShows()
-
-    fun insert(show: TvShowsRoomEntity){
-        executorService.execute { mShowsStarredDao.insert(show) }
+    suspend fun delShows(show: TvShowsRoomEntity){
+        showDao.delete(show)
     }
 
-    fun delete(show: TvShowsRoomEntity){
-        executorService.execute { mShowsStarredDao.delete(show) }
-    }
 
 }
