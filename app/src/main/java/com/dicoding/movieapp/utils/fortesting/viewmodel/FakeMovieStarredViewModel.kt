2a145b098027b63a68193dfbd264e4991fb4ebd3
briancatraguna <briancatraguna@rocketmail.com>
2021-05-25 01:21:38 +0700

    Fixed movie starred view model test

--- app/src/main/java/com/dicoding/movieapp/utils/fortesting/viewmodel/FakeMovieStarredViewModel.kt
+++ app/src/main/java/com/dicoding/movieapp/utils/fortesting/viewmodel/FakeMovieStarredViewModel.kt
@@ -1,4 +1,30 @@
 package com.dicoding.movieapp.utils.fortesting.viewmodel
 
-class FakeMovieStarredViewModel {
+import androidx.lifecycle.MutableLiveData
+import com.dicoding.movieapp.data.source.local.room.movies.MoviesRoomEntity
+import com.dicoding.movieapp.utils.DataDummy
+import com.dicoding.movieapp.utils.fortesting.repository.FakeFavoriteMovieRepository
+
+class FakeMovieStarredViewModel () {
+
+    private var data: MutableLiveData<ArrayList<MoviesRoomEntity>>
+    private var repository: FakeFavoriteMovieRepository
+
+    init {
+        repository = FakeFavoriteMovieRepository()
+        data = repository.getReadAllMovies()
+    }
+
+    fun getReadAllMovies(): MutableLiveData<ArrayList<MoviesRoomEntity>> {
+        return data
+    }
+
+    fun addMovie(){
+        repository.addMovie()
+    }
+
+    fun delMovieById(){
+        repository.delMovieById()
+    }
+
 }
--- app/src/test/java/com/dicoding/movieapp/searchresult/detailmovie/MovieStarredViewModelTest.kt
+++ app/src/test/java/com/dicoding/movieapp/searchresult/detailmovie/MovieStarredViewModelTest.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.Observer
 import com.dicoding.movieapp.data.source.local.room.movies.MoviesRoomEntity
 import com.dicoding.movieapp.data.source.local.room.shows.TvShowsRoomEntity
 import com.dicoding.movieapp.utils.DataDummy
+import com.dicoding.movieapp.utils.fortesting.viewmodel.FakeMovieStarredViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -21,18 +22,17 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class MovieStarredViewModelTest {
     private lateinit var dataDummy: DataDummy
-    private lateinit var viewModel: MovieStarredViewModel
+    private lateinit var viewModel: FakeMovieStarredViewModel
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Mock
-    private lateinit var moviesObserver: Observer<List<MoviesRoomEntity>>
+    private lateinit var moviesObserver: Observer<ArrayList<MoviesRoomEntity>>
 
     @Before
     fun init(){
-        val application = Application()
-        viewModel = MovieStarredViewModel(application)
+        viewModel = FakeMovieStarredViewModel()
         dataDummy = DataDummy
     }
 
@@ -43,13 +43,12 @@ class MovieStarredViewModelTest {
         val listMoviesExpectation = MutableLiveData<List<MoviesRoomEntity>>()
         listMoviesExpectation.value = dummyListMovies
 
-        //Reality
-        `when`(viewModel.readAllMovies).thenReturn(listMoviesExpectation)
-        val listMoviesReality = viewModel.readAllMovies
-        assertNotNull(listMoviesReality.value)
-        assertEquals(listMoviesExpectation.value,listMoviesReality.value)
+        //Actual
+        val listMoviesActual = viewModel.getReadAllMovies()
+        assertNotNull(listMoviesActual)
+        assertEquals(listMoviesExpectation.value,listMoviesActual.value)
 
-        viewModel.readAllMovies.observeForever(moviesObserver)
+        viewModel.getReadAllMovies().observeForever(moviesObserver)
         verify(moviesObserver).onChanged(dummyListMovies)
     }
 
@@ -57,20 +56,17 @@ class MovieStarredViewModelTest {
     fun addMovie() {
         //Expectation
         val dummyListMovies = dataDummy.getFakeMovieRoomDatabase()
-        val listMoviesExpectation = MutableLiveData<List<MoviesRoomEntity>>()
+        val listMoviesExpectation = MutableLiveData<ArrayList<MoviesRoomEntity>>()
         listMoviesExpectation.value = dummyListMovies
+        listMoviesExpectation.value!!.add(dataDummy.getMovieRoomEntity())
 
-        val dummyMovieAdded = dataDummy.getMovieRoomEntity()
+        //Actual
+        var listMoviesActual = viewModel.getReadAllMovies()
+        viewModel.addMovie()
+        assertNotNull(listMoviesActual)
+        assertEquals(listMoviesExpectation.value,listMoviesActual.value)
 
-        `when`(viewModel.addMovie(dummyMovieAdded)).then {
-            `when`(viewModel.readAllMovies).thenReturn(listMoviesExpectation)
-        }
-        viewModel.addMovie(dummyMovieAdded)
-        val listMoviesReality = viewModel.readAllMovies
-        assertNotNull(listMoviesReality.value)
-        assertEquals(listMoviesExpectation.value,listMoviesReality.value)
-
-        viewModel.readAllMovies.observeForever(moviesObserver)
+        viewModel.getReadAllMovies().observeForever(moviesObserver)
         verify(moviesObserver).onChanged(dummyListMovies)
     }
 
@@ -78,20 +74,18 @@ class MovieStarredViewModelTest {
     fun delMovieById() {
         //Expectation
         val dummyListMovies = dataDummy.getFakeMovieRoomDatabase()
-        val listMoviesExpectation = MutableLiveData<List<MoviesRoomEntity>>()
+        val listMoviesExpectation = MutableLiveData<ArrayList<MoviesRoomEntity>>()
         listMoviesExpectation.value = dummyListMovies
+        listMoviesExpectation.value!!.removeAt(0)
 
-        val dummyMovieId = 101
+        //Actual
+        var listMoviesActual = viewModel.getReadAllMovies()
+        viewModel.delMovieById()
+        assertNotNull(listMoviesActual)
+        assertEquals(listMoviesExpectation.value,listMoviesActual.value)
 
-        `when`(viewModel.delMovieById(dummyMovieId)).then {
-            `when`(viewModel.readAllMovies).thenReturn(listMoviesExpectation)
-        }
-        viewModel.delMovieById(dummyMovieId)
-        val listMoviesReality = viewModel.readAllMovies
-        assertNotNull(listMoviesReality.value)
-        assertEquals(listMoviesExpectation.value,listMoviesReality.value)
-
-        viewModel.readAllMovies.observeForever(moviesObserver)
+        viewModel.getReadAllMovies().observeForever(moviesObserver)
         verify(moviesObserver).onChanged(dummyListMovies)
+
     }
 }
